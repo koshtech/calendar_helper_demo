1f7fcc5cae065174edad00f60405abb6a9efe5d8
Fernando Kosh <kosh.linux@gmail.com>
2010-11-28 02:23:10 -0200

    Adding tableless calendar. :-D

--- app/controllers/calendar_helper_demo_controller.rb
+++ app/controllers/calendar_helper_demo_controller.rb
@@ -4,10 +4,11 @@ class CalendarHelperDemoController < ApplicationController
   end
   
   def div_calendar_text_and_link
-    @listOfSpecialDays = [ '2010-11-25'.to_date, '2010-11-29'.to_date ]
-    @listOfSpecialDetails = {
-      '2010-11-25' => [:day => '2010-11-25', :label => "<br />Text with<br /> <a href='#'>link</a>."], 
-      '2010-11-29' => [:day => '2010-11-29']
+    @listOfSpecialDays = [ '2010-11-15'.to_date, '2010-11-25'.to_date, '2010-12-03'.to_date ]
+    @listOfSpecialDetails = { 
+      '2010-11-15' => [:day => '2010-11-15', :label => "<a href='#'>Test one</a>"], 
+      '2010-11-25' => [:day => '2010-11-25', :label => "<a href='#'>Holiday</a>"],
+      '2010-12-03' => [:day => '2010-12-03', :label => "<a href='#'>Test three</a>"]
     }
   end 
   
